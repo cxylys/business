4617dbda2603df37a166492b727153ea9d5e8de3
liuyang <cxylys@163.com>
2020-09-21 18:09:20 +0800

    fix 版本

---
+++ miqilin-绝对路径版本/js/common.js
@@ -0,0 +1,114 @@
+function init() {
+  var video1 = new Swiper('.video-1', {
+    direction: 'horizontal', // 水平切换选项
+    // loop: true, // 循环模式选项
+    // autoplay: true,
+    pagination: {
+      el: '.swiper-pagination-video-1',
+      clickable: true,
+      // type: 'custom',
+      bulletClass: 'my-bullet', //需设置.my-bullet样式
+      bulletActiveClass: 'my-bullet-active',
+    },
+    on: {
+      slideChangeTransitionStart: function () {
+        var myVideo1 = document.getElementById('video-play-aq-1') //视频1
+        var myVideo2 = document.getElementById('video-play-aq-2') //视频2
+        var myVideo3 = document.getElementById('video-play-aq-3') //视频3
+        var index = this.activeIndex
+        if (index == 0) {
+          myVideo2.pause()
+        }
+        if (index == 1) {
+          myVideo1.pause()
+          myVideo3.pause()
+        }
+        if (index == 2) {
+          myVideo2.pause()
+        }
+      },
+    },
+  })
+  var video3 = new Swiper('.video-3-3', {
+    direction: 'horizontal', // 水平切换选项
+    loop: true, // 循环模式选项
+    autoplay: true,
+    pagination: {
+      el: '.swiper-pagination-video-3',
+      clickable: true,
+      // type: 'custom',
+      bulletClass: 'my-bullet', //需设置.my-bullet样式
+      bulletActiveClass: 'my-bullet-active',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-3',
+      prevEl: '.swiper-button-prev-3',
+    },
+  })
+  var video4 = new Swiper('.video-4-4', {
+    direction: 'horizontal', // 水平切换选项
+    loop: true, // 循环模式选项
+    // autoplay: true,
+    pagination: {
+      el: '.swiper-pagination-video-4',
+      clickable: true,
+      // type: 'custom',
+      bulletClass: 'my-bullet', //需设置.my-bullet样式
+      bulletActiveClass: 'my-bullet-active',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-4',
+      prevEl: '.swiper-button-prev-4',
+    },
+  })
+  $('.swiper-button-prev-4').click(function () {
+    video4.slidePrev()
+  })
+  $('.swiper-button-next-4').click(function () {
+    video4.slideNext()
+  })
+  $('.swiper-button-prev-3').click(function () {
+    video3.slidePrev()
+  })
+  $('.swiper-button-next-3').click(function () {
+    video3.slideNext()
+  })
+}
+
+$(function () {
+  init()
+  // $('.top-video .video .btn').click(function () {
+  //   // var url = $(this).attr('data-url')
+  //   $('#video-play').attr('src', 'images/video_1.mp4')
+  //   $('.video-wrapper').show()
+  //   document.getElementById('video-play').play()
+  // })
+  // $('.video-wrapper .close').click(function () {
+  //   $('.video-wrapper').hide()
+  //   document.getElementById('video-play').pause()
+  //   document.getElementById('video-play').currentTime = 0
+  // })
+  //顶部导航
+  $('.kv .buttons a').click(function () {
+    var index = $(this).attr('data-index')
+    if (index != undefined) {
+      $('html,body').animate({
+        scrollTop: $('#nav-' + index).offset().top - 30,
+      })
+    }
+  })
+  $('.top-video .video .btn').click(function () {
+    document.getElementById('video-play-top').play()
+    $(this).hide()
+  })
+  // 活动规则
+  $('.kv .buttons .comm-desc-btn').click(function () {
+    $('.comm-desc-modal').show()
+    $('body').css({ overflow: 'hidden' })
+  })
+  //关闭 活动规则
+  $('.comm-desc-modal .comm-rule-box .close-btn').click(function () {
+    $('.comm-desc-modal').hide()
+    $('body').css({ overflow: '' })
+  })
+})
