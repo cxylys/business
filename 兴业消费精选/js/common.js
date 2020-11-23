68e2bfb6d7a0040eee465ffa73290358c69bd8c1
liuyang <cxylys@163.com>
2020-11-23 10:39:35 +0800

    fix 1039

--- 兴业消费精选/js/common.js
+++ 兴业消费精选/js/common.js
@@ -1,3 +1,5 @@
+var bodyHeigth = 0
+var kvHeight = 0
 function init() {
   if (sessionStorage.getItem('slidenum')) {
     var index = sessionStorage.getItem('slidenum')
@@ -7,6 +9,11 @@ function init() {
     $('.menu-list li').eq(0).addClass('on')
     $('.list-data .item').eq(0).show()
   }
+  bodyHeigth = $('body').height()
+  kvHeight = $('.kv').height()
+  $('.content').height(bodyHeigth - kvHeight)
+  console.log(bodyHeigth)
+  console.log(kvHeight)
 }
 
 $(function () {
