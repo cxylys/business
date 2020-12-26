899a97847da8c242efb03590a8a0958831b13915
liuyang <cxylys@163.com>
2020-12-27 01:52:17 +0800

    fix 0152

--- 景顺长城余广集合页/js/common.js
+++ 景顺长城余广集合页/js/common.js
@@ -2,8 +2,8 @@ var navSwiper
 function init() {
   var myBannerSwiper = new Swiper('.banner', {
     direction: 'horizontal', // 水平切换选项
-    loop: true, // 循环模式选项
-    autoplay: true,
+    // loop: true, // 循环模式选项
+    // autoplay: true,
     // pagination: {
     //   el: '.swiper-pagination-banner',
     // },
