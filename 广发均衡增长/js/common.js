b173c76eca3ad0dbbbb883a6addc279deabb4992
liuyang <cxylys@163.com>
2021-01-12 16:03:52 +0800

    fix 1603

--- 广发均衡增长/js/common.js
+++ 广发均衡增长/js/common.js
@@ -146,6 +146,7 @@ $(function () {
   })
   $('.video-wrapper .close').click(function () {
     $('.video-wrapper').hide()
+    $('body').css('overflow', '')
     document.getElementById('video-play').pause()
     // document.getElementById('video-play').currentTime = 0
   })
