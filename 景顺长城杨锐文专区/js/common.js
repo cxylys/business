7aeb4669936dbb6f2140f00a79b27002ccafb5cd
liuyang <cxylys@163.com>
2021-02-04 13:57:18 +0800

    fix 1357

--- 景顺长城杨锐文专区/js/common.js
+++ 景顺长城杨锐文专区/js/common.js
@@ -40,28 +40,16 @@ $(function () {
     document.getElementById('video-play').pause()
     // document.getElementById('video-play').currentTime = 0
   })
-  // 新基来袭视频
-  $('.xinji-btn').click(function () {
-    $('.xinji-modal').show()
+  // 产品特点视频
+  $('.product-btn').click(function () {
+    $('.product-modal').show()
     $('body').scrollTop(0)
     $('body').css('overflow', 'hidden')
-    myListener('产品专区', '视频集锦', '新基来袭', 1)
+    myListener('首页', '视频集锦', '产品特点', 1)
   })
-  $('.xinji-modal .close').click(function () {
-    $('.xinji-modal').hide()
+  $('.product-modal .close').click(function () {
+    $('.product-modal').hide()
     $('body').css('overflow', '')
-  })
-  $('.xinji-modal ul li a').click(function () {
-    var url = $(this).attr('data-url')
-    var level1 = $(this).attr('data-parent')
-    var level2 = $(this).attr('data-child')
-    var name = $(this).attr('data-name')
-    myListener(level1, level2, name, 1)
-    if (url) {
-      $('#video-play').attr('src', url)
-      $('.video-wrapper').show()
-      document.getElementById('video-play').play()
-    }
   })
 
   // 投资方法论视频
@@ -69,57 +57,11 @@ $(function () {
     $('.touzi-modal').show()
     $('body').scrollTop(0)
     $('body').css('overflow', 'hidden')
-    myListener('詹成专区', '视频集锦', '投资方法论', 1)
+    myListener('首页', '视频集锦', '投资观点', 1)
   })
   $('.touzi-modal .close').click(function () {
     $('.touzi-modal').hide()
     $('body').css('overflow', '')
-  })
-  $('.touzi-modal ul li a').click(function () {
-    var url = $(this).attr('data-url')
-    var level1 = $(this).attr('data-parent')
-    var level2 = $(this).attr('data-child')
-    var name = $(this).attr('data-name')
-    var link = $(this).attr('data-link')
-    myListener(level1, level2, name, 1)
-    if (url) {
-      if (link) {
-        location.href = url
-      } else {
-        $('#video-play').attr('src', url)
-        $('.video-wrapper').show()
-        document.getElementById('video-play').play()
-      }
-    }
-  })
-
-  // 理念观点视频
-  $('.linian-btn').click(function () {
-    $('.linian-modal').show()
-    $('body').scrollTop(0)
-    $('body').css('overflow', 'hidden')
-    myListener('詹成专区', '视频集锦', '理念观点', 1)
-  })
-  $('.linian-modal .close').click(function () {
-    $('.linian-modal').hide()
-    $('body').css('overflow', '')
-  })
-  $('.linian-modal ul li a').click(function () {
-    var url = $(this).attr('data-url')
-    var level1 = $(this).attr('data-parent')
-    var level2 = $(this).attr('data-child')
-    var name = $(this).attr('data-name')
-    var link = $(this).attr('data-link')
-    myListener(level1, level2, name, 1)
-    if (url) {
-      if (link) {
-        location.href = url
-      } else {
-        $('#video-play').attr('src', url)
-        $('.video-wrapper').show()
-        document.getElementById('video-play').play()
-      }
-    }
   })
 
   //监测
@@ -135,6 +77,22 @@ $(function () {
   })
 
   $('.business ul li a').click(function () {
+    var url = $(this).attr('data-url')
+    var l1 = $(this).attr('data-level1')
+    var l2 = $(this).attr('data-level2')
+    var l3 = $(this).attr('data-level3')
+    myListener(l1, l2, l3, 1, url)
+  })
+
+  $('.article ul li a').click(function () {
+    var url = $(this).attr('data-url')
+    var l1 = $(this).attr('data-level1')
+    var l2 = $(this).attr('data-level2')
+    var l3 = $(this).attr('data-level3')
+    myListener(l1, l2, l3, 1, url)
+  })
+
+  $('.video-modal ul li a').click(function () {
     var url = $(this).attr('data-url')
     var l1 = $(this).attr('data-level1')
     var l2 = $(this).attr('data-level2')
